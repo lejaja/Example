2e4ef00ddb06ba8962a9c1a38d1638552fafc763
Charly Ngoukam <ngoukamcharly@yahoo.fr>
2018-01-21 21:50:22 +0100

    Ajout des Methodes additionner et soustraire

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,20 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Charly");
+		Hello hello=new Hello();
+		int h1=hello.additionner(10,8);
+		int h2=hello.soustraire(10,8);
+		System.out.println(h1);
+		System.out.println(h2);
 
+	}
+	
+	public int additionner(int a, int b){
+		return a+b;
+	}
+	
+	public int soustraire(int a, int b){
+		return a-b;
 	}
 
 }
